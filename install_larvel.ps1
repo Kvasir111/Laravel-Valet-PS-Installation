927b7609c61c5343c34311b90543ce2c129b3d30
Ryan Rowe <rrowe222@gmail.com>
2019-03-18 12:52:51 -0500

    Update install_larvel.ps1
    Removed useless function so adding to the path works correctly

--- install_larvel.ps1
+++ install_larvel.ps1
@@ -1,45 +1,18 @@
-﻿
-Out-Null
-function Add-EnvPath {
-    param(
-        [Parameter(Mandatory=$true)]
-        [string] $Path,
-
-        [ValidateSet('Machine', 'User', 'Session')]
-        [string] $Container = 'Session'
-    )
-
-    if ($Container -ne 'Session') {
-        $containerMapping = @{
-            Machine = [EnvironmentVariableTarget]::Machine
-            User = [EnvironmentVariableTarget]::User
-        }
-        $containerType = $containerMapping[$Container]
-
-        $persistedPaths = [Environment]::GetEnvironmentVariable('Path', $containerType) -split ';'
-        if ($persistedPaths -notcontains $Path) {
-            $persistedPaths = $persistedPaths + $Path | where { $_ }
-            [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join ';', $containerType)
-        }
-    }
-
-    $envPaths = $env:Path -split ';'
-    if ($envPaths -notcontains $Path) {
-        $envPaths = $envPaths + $Path | where { $_ }
-        $env:Path = $envPaths -join ';'
-    }
-}
-
+$phpPath = c:\PHP\
 cd $HOME
 cd .\Downloads\
 wget -UseBasicParsing https://windows.php.net/downloads/releases/php-7.3.3-nts-Win32-VC15-x64.zip -OutFile PHP.zip
 wget -UseBasicParsing https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe -OutFile VS2015.exe
 .\VS2015.exe /s
-Expand-Archive PHP.zip -DestinationPath c:\PHP\
-cd c:\PHP\
+cd c:\
+mkdir $phpPath
+Expand-Archive PHP.zip -DestinationPath $phpPath
+cd $phpPath
 del C:\PHP\php.ini
-Add-EnvPath c:\php
 wget -UseBasicParsing https://hastebin.com/raw/ukejavikad -OutFile .\"php.ini"
+$oldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+$newPath=$oldPath+’;C:\PHP\’
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath
 mkdir 'C:\Program Files\composer'
 cd 'C:\Program Files\composer'
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -49,8 +22,11 @@ php -r "unlink('composer-setup.php');"
 $compBS = '@php "%~dp0composer.phar" %*'
 Out-File -FilePath .\composer.bat
 $compBS | Set-Content .\composer.bat -Encoding Ascii
-Add-EnvPath "C:\Program Files\composer\"
+$oldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+$newPath=$oldPath+’;C:\Program Files\composer’
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath
 php composer.phar global require laravel/installer
-Add-EnvPath "$HOME\AppData\Roaming\Composer\vendor\bin"
-php -v
-$Env:path += ";'C:\Program Files\composer\'"
+$oldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+$newPath=$oldPath+’;$HOME\AppData\Roaming\Composer\vendor\bin’
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath
+
